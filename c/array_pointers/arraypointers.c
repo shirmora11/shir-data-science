e4ae1d1364e06e38ae7d2559708579720a6a47c9
shir.moraidi <shirmoraidi@gmail.com>
2026-01-12 08:01:54 +0200

    updated

--- c/array_pointers/arraypointers.c
+++ c/array_pointers/arraypointers.c
@@ -55,7 +55,7 @@ void josephus_n2() {
 int josephus(int n, int k) {
     if (n == 1)
         return 1;
-    else
+    else // Recursive case
         return (josephus(n - 1, k) + k - 1) % n + 1;
 }
 
