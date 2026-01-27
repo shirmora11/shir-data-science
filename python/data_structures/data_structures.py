b7e60b57933c404ad8dbc7dce0908eeb59f07c75
shir.moraidi <shirmoraidi@gmail.com>
2026-01-27 11:42:06 +0200

    solved

---
+++ python/data_structures/data_structures.py
@@ -0,0 +1,48 @@
+
+
+
+def remove_list(list):
+    """function that clears list"""
+    return list.clear()
+def string_to_letters(string):
+    """function that breakdown string to letters"""
+    answer = {}
+    for char in string:
+        answer[char] = answer.get(char, 0)+1
+    print(f"{answer}")
+
+def two_lists(first, second):
+    """function that turns two lists to one with all the
+    elements that were in both"""
+    third = [item for item in first if item in second]
+def unique_list(dict):
+    """function that recive a dictionary and return a list with all unique values"""
+    list = (set(dict.values()))
+    print(list)
+    return list
+def left_rotation(list):
+    """function that receive a list and rotate it left one time"""
+    new_list = list[::-1]
+def jump_remove(list):
+    """a function that jumps 2 places every time until the list is empty"""
+    while (len(list) != 0):
+        for item in list[::2]:
+            if item in list:
+                print(item)
+                list.remove(item)
+    return list
+def dict_to_list(dict):
+    """a function that receive a dict and turns the key value pair to a list of tuples"""
+    listof = tuple(dict.items())
+    print(listof)
+def max_min(dict):
+    """function that gives the max and min of the dictionary"""
+    ma = max(dict.values())
+    mi = min(dict.values())
+    print(ma,mi)
+def not_common_letters(first,second):
+    """a function that prints all the letters of the first string that wasn't used in the second one"""
+    third = [item for item in first if item not in second]
+    print(third)
+
+
---
+++ python/data_structures/main.py
@@ -0,0 +1,11 @@
+import data_structures as ds
+
+ds.remove_list([1,2,3,4])
+ds.string_to_letters("shir")
+ds.two_lists([1,2,34,4],[5,6,6,2,1])
+ds.unique_list({1:3,2:2,3:3,4:5,5:5})
+ds.left_rotation([1,2,3,4,5])
+ds.jump_remove([1, 2, 3, 4, 5, 6])
+ds.dict_to_list({1:1,2:2,3:3,4:4,5:5})
+ds.max_min({1:2, 2:3, 3:5, 4:0})
+ds.not_common_letters("shir", "adi")
